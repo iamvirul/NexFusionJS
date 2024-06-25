4b15c5d6e82a7573d2f125a80e18727f3bb0af35
Virul Nirmala Wickramasinghe <89099391+iamvirul@users.noreply.github.com>
2024-06-25 16:27:09 +0530

    Create _app.d.ts

---
+++ dist/client/pages/_app.d.ts
@@ -0,0 +1,4 @@
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
+declare function MyApp({ Component, pageProps }: AppProps): import("react/jsx-runtime").JSX.Element;
+export default MyApp;
