45ef5c0a1a8368e618e421b5a63445901b5309ad
Virul Nirmala Wickramasinghe <89099391+iamvirul@users.noreply.github.com>
2024-06-25 16:28:04 +0530

    Create index.js

---
+++ dist/client/pages/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const jsx_runtime_1 = require("react/jsx-runtime");
+const HomePage = () => {
+    return ((0, jsx_runtime_1.jsx)("div", { className: "flex justify-center items-center h-screen bg-gray-100", children: (0, jsx_runtime_1.jsx)("h1", { className: "text-3xl font-bold underline", children: "Welcome to FusionJS" }) }));
+};
+exports.default = HomePage;
+//# sourceMappingURL=index.js.map
