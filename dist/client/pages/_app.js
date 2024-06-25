28a1fbc11e6368acdbef5b10bd28d6e0c3c66d7a
Virul Nirmala Wickramasinghe <89099391+iamvirul@users.noreply.github.com>
2024-06-25 16:27:57 +0530

    Create _app.js

---
+++ dist/client/pages/_app.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const jsx_runtime_1 = require("react/jsx-runtime");
+require("../styles/globals.css");
+function MyApp({ Component, pageProps }) {
+    return (0, jsx_runtime_1.jsx)(Component, Object.assign({}, pageProps));
+}
+exports.default = MyApp;
+//# sourceMappingURL=_app.js.map
